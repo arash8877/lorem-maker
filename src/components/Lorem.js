df4d6b32e335c623cc9654ab97b5eac61603e559
Elaheh <elah0048@stud.kea.dk>
2022-11-12 15:12:45 +0100

    done

--- src/components/Lorem.js
+++ src/components/Lorem.js
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Lorem.css";
+import data from "./data";
 
 const Lorem = () => {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleChange = (event) => {
+    setCount(event.target.value);
+     console.log(count)
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let amount = parseInt (count)
+
+    if(count <= 0) {amount = 0};
+    if(count > 4) {amount = 4}
+    
+    setText(data.slice(0,amount))
+  };
+       
   return (
     <div className="container">
       <h4>Lorem Maker</h4>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Number of Paragraphs:</label>
-        <input type='number'/>
+        <input 
+        type="number" 
+        onChange={handleChange}
+        value={count} />
         <button type="submit">Make</button>
       </form>
-      <p>first paragraph</p>
+      <article>
+        {text.map((item) => {return <p>{item.description}</p>}) }
+      </article>
     </div>
   );
 };
---
+++ src/components/data.js
@@ -0,0 +1,15 @@
+const data=[
+    {_id:1,
+    description:' It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution'},
+
+    {_id:2,
+    description: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum'},
+
+    {_id:3,
+    description: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly'},
+
+    {_id:4,
+    description: 'the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum'},
+]
+
+export default data
